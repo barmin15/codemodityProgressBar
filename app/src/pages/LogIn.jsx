e7dd629e65bc47dd4b2c7ff3256d792ab1bb387d
barmin15 <115711430+barmin15@users.noreply.github.com>
2023-11-21 19:43:05 +0100

    refactor: tags

--- app/src/pages/LogIn.jsx
+++ app/src/pages/LogIn.jsx
@@ -5,11 +5,13 @@ import "../css/login.css"
 
 export default function Login() {
     const [isActive, setIsActive] = useState(false);
-    const [username, setUsername] = useState(false);
+    const [username, setUsername] = useState(null);
 
     function onLogin(e) {
         e.preventDefault();
+
         setIsActive(true)
+
         setTimeout(()=>{
             setIsActive(false)
         },5000)
@@ -17,12 +19,12 @@ export default function Login() {
 
     return <div>
         <LoginBar username={username} isActive={isActive} />
-        <div class="login-container">
+        <div className="login-container">
             <div>
-                <label for="username">Username</label>
+                <label htmlFor="username">Username</label>
                 <input placeholder='enter username' type="text" id="username" name="username" onChange={(e) => setUsername(e.target.value)} />
 
-                <label for="password">Password</label>
+                <label htmlFor="password">Password</label>
                 <input placeholder='enter password' type="password" id="password" name="password" />
 
                 <button onClick={onLogin}>Login</button>
